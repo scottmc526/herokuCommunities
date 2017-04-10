a14589f5afe71bc15438c3415cb9b073bac4cc0f
Scott Christensen <scottmc526@gmail.com>
2017-04-10 14:05:31 -0600

    working master copy

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,15 @@
-var app = angular.module('herokuCommunities', ['auth0.lock', 'angular-jwt', 'ui.router']);
 
-app.config(function($stateProvider, lockProvider, $urlRouterProvider) {
+var clientId = 'WqDAlwLEQvV2NqfiEaWBa7sWg9mE6A9f';
+var domain = 'app66452704.auth0.com';
+
+
+var app = angular.module('herokuCommunities', ['ui.router']);
+
+app.config(function($stateProvider) {
   $stateProvider
   .state('login', {
     url:'/',
     controller: 'loginController',
     templateUrl: 'partials/login.html'
   })
-
-
 })
