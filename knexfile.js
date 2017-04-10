5b85980137937fcd23c5516a66234b788190a162
Scott Christensen <scottmc526@gmail.com>
2017-04-10 14:17:43 -0600

    knex setup

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+require('dotenv').load();
+
+module.exports = {
+  development: {
+      client: 'pg',
+      connection: 'postgres://localhost/heroku_comm'
+    },
+
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL + '?ssl=true'
+    }
+};
