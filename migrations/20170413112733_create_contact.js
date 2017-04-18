47ef85fc145d7137178b34fe9eb800b2906a4f88
Scott Christensen <scottmc526@gmail.com>
2017-04-18 14:40:23 -0600

    schema

--- migrations/20170413112733_create_contact.js
+++ migrations/20170413112733_create_contact.js
@@ -1,9 +1,14 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('contact', function(t) {
+    t.boolean('isdeleted');
     t.increments();
+    t.string('_hc_lastop');
+    t.string('_hc_err');
     t.string('sfid');
+    t.string('createddate')
     t.string('name');
+    t.string('systemmodstamp')
     t.string('email');
   })
 };
