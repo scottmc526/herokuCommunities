63430f788b685584fd433f79e16316c0f7e4ba08
Scott Christensen <scottmc526@gmail.com>
2017-04-19 12:45:50 -0600

    contact record displays, logout function

--- public/javascripts/controllers/contactRecordController.js
+++ public/javascripts/controllers/contactRecordController.js
@@ -1,3 +1,15 @@
-app.controller('contactRecordController', function ($scope, $cookies, userInfo) {
-  $scope.test = 'test';
+app.controller('contactRecordController', function ($scope, $cookies, $window, userInfo) {
+  $scope.userId = $cookies.get('user');
+  $scope.name = '';
+  $scope.email = ''
+
+  userInfo.getContactRecord($scope.userId).then(function(result) {
+    $scope.email = result.data[0].email;
+    $scope.name = result.data[0].name;
+  })
+
+  $scope.logOut = function() {
+    $cookies.remove('user');
+    $window.location.href = '/#/';
+  }
 })
